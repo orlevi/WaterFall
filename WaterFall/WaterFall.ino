83780286d01b2349baa90036187bb16436cd4d8e
netanelf <netanelf@gmail.com>
2017-01-12 22:50:12 +0200

    added some patterns

--- WaterFall/WaterFall.ino
+++ WaterFall/WaterFall.ino
@@ -4,7 +4,7 @@
 
 #define NUM_OF_COLUMNS 8
 
-#define PIXEL_TIME 350
+#define PIXEL_TIME 20 //350
 //------------------------------------------------------------------//
 
 void setup() {
@@ -18,12 +18,16 @@ void setup() {
 
 void loop() {
   pictureIt();
+  delay(1000);
+  pictureIt2();
+  delay(1000);
+  
 }
 
 
 void loadLine(long line) {
   for (int i = 0; i < NUM_OF_COLUMNS; i++) {
-    digitalWrite(DATA, (line >> i) & 1);
+    digitalWrite(DATA, !((line >> i) & 1));
     oneClk();
   }
   latchIt();
@@ -41,7 +45,125 @@ void latchIt() {
 }
 
 void pictureIt() {
-  loadLine(0xFF);
+  /*
+  loadLine(0x1);
+  loadLine(0x2);
+  loadLine(0x4);
+  loadLine(0x8);
+  loadLine(0x10);
+  loadLine(0x20);
+  loadLine(0x40);
+  loadLine(0x80);
+  loadLine(0x40);
+  loadLine(0x20);
+  loadLine(0x10);
+  loadLine(0x8);
+  loadLine(0x4);
+  loadLine(0x2);
+  */
+  /*
+  loadLine(0x81);
   loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x42);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x24);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x18);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x24);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x42);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x81);
+  */
+
+  /*
+  loadLine(0xFF);
+  loadLine(0xFF);
+  //loadLine(0xFF);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  */
+  loadLine(0x1);
+  loadLine(0x3);
+  loadLine(0x7);
+  loadLine(0xf);
+  loadLine(0x1f);
+  loadLine(0x3f);
+  loadLine(0x7f);
+  loadLine(0xff);
+  loadLine(0x0);
+  
+}
+void pictureIt2() {
+  /*
+  loadLine(0x1);
+  loadLine(0x2);
+  loadLine(0x4);
+  loadLine(0x8);
+  loadLine(0x10);
+  loadLine(0x20);
+  loadLine(0x40);
+  loadLine(0x80);
+  loadLine(0x40);
+  loadLine(0x20);
+  loadLine(0x10);
+  loadLine(0x8);
+  loadLine(0x4);
+  loadLine(0x2);
+  */
+  /*
+  loadLine(0x81);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x42);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x24);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x18);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x24);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x42);
+  loadLine(0x0);
+  //loadLine(0x0);
+  loadLine(0x81);
+  */
+
+  /*
+  loadLine(0xFF);
+  loadLine(0xFF);
+  //loadLine(0xFF);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  loadLine(0x0);
+  */
+  loadLine(0x80);
+  loadLine(0xc0);
+  loadLine(0xe0);
+  loadLine(0xf0);
+  loadLine(0xf8);
+  loadLine(0xfc);
+  loadLine(0xfe);
+  loadLine(0xff);
+  loadLine(0x0);
+  
 }
 
